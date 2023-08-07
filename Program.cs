bfe1a6558c7e36d30f48785fc54c952212562a23
Matheus Canuto <matheushenriquecanuto77@gmail.com>
2023-08-06 21:06:39 -0300

    Fatorial com recursividade

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AtividadeDeAEDs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Essa é a formula que define um fatorial de um número
+            //n! = n * (n - 1)!
+            //Fatorando essa função
+            //n! = n * (n - 1) * (n-2) * (n-3) ... * 1
+            int fatorialComFuncao(int n)
+            {   
+                if(n == 0)
+                    return 1;
+                else
+                    return n * fatorialComFuncao(n - 1);
+            }
+            int fat = fatorialComFuncao(5);
+            Console.WriteLine(fat);
+            Console.ReadKey();
+
+            void fatorialComFor()
+            {
+                Console.WriteLine("Digite o número que você deseja um fatorial: ");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(num);
+                int aux = 1;
+                for (int i = 1; i < num; i++)
+                {
+                    aux += i * aux;
+                }
+
+                Console.WriteLine("Esse é o valor do fatorial: " + aux);
+                Console.ReadKey();
+            }
+        }
+    }
+}
